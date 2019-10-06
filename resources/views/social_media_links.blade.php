2ac95257c4a19b258ebe8094b3a483d1c9220392
Sofian Mourabit <sofianmourabit@gmail.com>
2019-10-06 20:08:48 +0200

    Reference public assets

--- resources/views/social_media_links.blade.php
+++ resources/views/social_media_links.blade.php
@@ -5,6 +5,12 @@ Share::currentPage($title)
 ->toTwitter()
 !!}
 
-@section('scripts')
-<script src="{{ asset('vendor/social-media-links/js/social-media-links.js') }}"></script>
-@endsection
+@push('css')
+    <link href="{{ asset('vendor/social-media-links/css/social-media-links.css') }}" 
+    rel="stylesheet" 
+    media="screen" 
+    type="text/css">
+@endpush
+@push('scripts')
+    <script src="{{ asset('vendor/social-media-links/js/social-media-links.js') }}"></script>
+@endpush
