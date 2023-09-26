03afb1551a6e0122be0a0e0a656a5a87377b1c20
Jyrki De Neve <jyrki.deneve@gmail.com>
2023-09-26 17:11:44 +0300

    Switch to Pest and allow L10

---
+++ tests/Feature/GenerateMultipleSocialMediaLinksTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Codedor\SocialMediaShare\Facades\Share;
+use Codedor\SocialMediaShare\Tests\TestCase;
+
+it('can generate multiple social media links', function () {
+    $result = (string) Share::currentPage('pageTitle')
+        ->toFacebook();
+
+    $expected = '<div id="js-social-media-share"><ul class="social-media-share"><li>
+    <a
+        href="https://www.facebook.com/sharer/sharer.php?u=http://localhost/"
+        class="js-social-media-share  js-track"
+        data-action="hit"
+        data-category="Share Facebook"
+        data-label="/"
+    >
+        <i class="fab fa-lg fa-facebook-square"></i>
+    </a>
+</li>
+
+</ul></div>';
+
+    $this->assertEquals($expected, $result);
+});
---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+use Codedor\SocialMediaShare\Tests\TestCase;
+
+uses(TestCase::class)->in('Feature');
