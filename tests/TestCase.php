3a2b2cb482878cacecbdb62f3b3a60a93a970dce
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-01-20 22:39:43 +0200

    Remove docblock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,6 @@ namespace Wotz\SocialMediaShare\Tests;
 
 use Wotz\SocialMediaShare\SocialMediaShareServiceProvider;
 
-/**
- * Class TestCase
- *
- * @author  Sofian Mourabit <sofian@codedor.be>
- *
- * @since   2019-10-02
- */
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
